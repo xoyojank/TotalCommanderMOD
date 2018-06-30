a3c5264e182fc34590cbb14c9dcedf6674869ec2
xoyojank <xoyojank@gmail.com>
2018-07-01 01:23:40 +0800

    ansi functions dll export remove absolute path

--- Yaz0/yaz0_wcx64.cpp
+++ Yaz0/yaz0_wcx64.cpp
@@ -1,17 +1,49 @@
 #include "wcxhead.h"
 #include "yaz0.h"
+#include <shlwapi.h>
 
 typedef struct
 {
+	CHAR ArchiveName[MAX_PATH];
 	WCHAR ArchiveNameW[MAX_PATH];
 	HANDLE ArchiveHandle;
 	BYTE* ArchiveData;
 	UINT PackSize;
 	UINT UnpackSize;
+	tChangeVolProc ChangeVolProc;
+	tProcessDataProc ProcessDataProc;
 	tChangeVolProcW ChangeVolProcW;
 	tProcessDataProcW ProcessDataProcW;
 } Yaz0Archive;
+//------------------------------------------------------------------------------
+HANDLE __stdcall OpenArchive(tOpenArchiveData* ArchiveData)
+{
+	HANDLE hArcFile = CreateFile(ArchiveData->ArcName, GENERIC_READ, FILE_SHARE_READ, 0, OPEN_EXISTING, 0, NULL);
+	if (INVALID_HANDLE_VALUE == hArcFile)
+	{
+		ArchiveData->OpenResult = E_EOPEN;
+		return 0;
+	}
+	DWORD bytesRead = 0;
 
+	UINT32 magic = 0;
+	if (!ReadFile(hArcFile, &magic, sizeof(magic), &bytesRead, NULL))
+	{
+		ArchiveData->OpenResult = E_EREAD;
+		return 0;
+	}
+	if (magic != '0zaY')
+	{
+		ArchiveData->OpenResult = E_UNKNOWN_FORMAT;
+		return 0;
+	}
+
+	Yaz0Archive* yaz0Archive = new Yaz0Archive();
+	ZeroMemory(yaz0Archive, sizeof(Yaz0Archive));
+	strcpy_s(yaz0Archive->ArchiveName, MAX_PATH, ArchiveData->ArcName);
+	yaz0Archive->ArchiveHandle = hArcFile;
+	return yaz0Archive;
+}
 //------------------------------------------------------------------------------
 HANDLE __stdcall OpenArchiveW(tOpenArchiveDataW* ArchiveData)
 {
@@ -41,7 +73,6 @@ HANDLE __stdcall OpenArchiveW(tOpenArchiveDataW* ArchiveData)
 	yaz0Archive->ArchiveHandle = hArcFile;
 	return yaz0Archive;
 }
-
 //------------------------------------------------------------------------------
 int __stdcall CloseArchive(HANDLE hArcData)
 {
@@ -55,7 +86,50 @@ int __stdcall CloseArchive(HANDLE hArcData)
 
 	return 0;
 }
+//------------------------------------------------------------------------------
+int __stdcall ReadHeader(HANDLE hArcData, tHeaderData* HeaderData)
+{
+	if (nullptr == hArcData || nullptr == HeaderData)
+		return E_BAD_DATA;
 
+	ZeroMemory(HeaderData, sizeof(tHeaderData));
+	Yaz0Archive* yaz0Archive = (Yaz0Archive*)hArcData;
+
+	if (yaz0Archive->ArchiveData)
+		return E_END_ARCHIVE;
+
+	yaz0Archive->PackSize = GetFileSize(yaz0Archive->ArchiveHandle, NULL);
+	SetFilePointer(yaz0Archive->ArchiveHandle, 0, NULL, FILE_BEGIN);
+	yaz0Archive->ArchiveData = new BYTE[yaz0Archive->PackSize];
+	DWORD bytesRead = 0;
+	if (!ReadFile(yaz0Archive->ArchiveHandle, yaz0Archive->ArchiveData, yaz0Archive->PackSize, &bytesRead, NULL))
+	{
+		return E_EREAD;
+	}
+	yaz0Archive->UnpackSize = yaz0_get_size(yaz0Archive->ArchiveData);
+	// get file time
+	INT archiveTime = 0;
+	FILETIME time;
+	GetFileTime(yaz0Archive->ArchiveHandle, &time, &time, &time);
+	FileTimeToDosDateTime(&time, ((WORD*)&archiveTime) + 1, (WORD*)&archiveTime);
+
+	strcpy_s(HeaderData->ArcName, MAX_PATH, yaz0Archive->ArchiveName);
+	strcpy_s(HeaderData->FileName, MAX_PATH, PathFindFileNameA(yaz0Archive->ArchiveName));
+	for (size_t i = strlen(HeaderData->FileName); i > 0; --i)
+	{
+		if (HeaderData->FileName[i] == '.')
+		{
+			HeaderData->FileName[i] = '\0';
+			break;
+		}
+	}
+	HeaderData->FileTime = archiveTime;
+	HeaderData->PackSize = yaz0Archive->PackSize;
+	HeaderData->UnpSize = yaz0Archive->UnpackSize;
+	HeaderData->FileAttr = FA_ARCHIVE;
+
+	return 0;
+}
 //------------------------------------------------------------------------------
 int __stdcall ReadHeaderExW(HANDLE hArcData, tHeaderDataExW* HeaderDataExW)
 {
@@ -84,7 +158,7 @@ int __stdcall ReadHeaderExW(HANDLE hArcData, tHeaderDataExW* HeaderDataExW)
 	FileTimeToDosDateTime(&time, ((WORD*)&archiveTime) + 1, (WORD*)&archiveTime);
 
 	wcscpy_s(HeaderDataExW->ArcName, MAX_PATH, yaz0Archive->ArchiveNameW);
-	wcscpy_s(HeaderDataExW->FileName, MAX_PATH, yaz0Archive->ArchiveNameW);
+	wcscpy_s(HeaderDataExW->FileName, MAX_PATH, PathFindFileNameW(yaz0Archive->ArchiveNameW));
 	for (size_t i = wcslen(HeaderDataExW->FileName); i > 0; --i)
 	{
 		if (HeaderDataExW->FileName[i] == L'.')
@@ -100,7 +174,36 @@ int __stdcall ReadHeaderExW(HANDLE hArcData, tHeaderDataExW* HeaderDataExW)
 
 	return 0;
 }
+//------------------------------------------------------------------------------
+int __stdcall ProcessFile(HANDLE hArcData, int Operation, char* DestPath, char* DestName)
+{
+	if (!hArcData)
+		return E_BAD_DATA;
+	Yaz0Archive* yaz0Archive = (Yaz0Archive*)hArcData;
 
+	if (Operation == PK_EXTRACT)
+	{
+		CHAR path[MAX_PATH * 2];
+		path[0] = 0;
+		if (DestPath)
+			strcpy_s(path, MAX_PATH * 2, DestPath);
+		if (DestName)
+		{
+			strcat_s(path, MAX_PATH * 2, path[0] ? "\\" : "");
+			strcat_s(path, MAX_PATH * 2, DestName);
+		}
+
+		HANDLE hFile = CreateFile(path, GENERIC_WRITE, FILE_SHARE_READ, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_ARCHIVE, NULL);
+		if (INVALID_HANDLE_VALUE == hFile)
+			return E_ECREATE;
+		BYTE* buffer = new BYTE[yaz0Archive->UnpackSize];
+		yaz0_decode(yaz0Archive->ArchiveData, buffer);
+		DWORD bytesWrite = 0;
+		if (!WriteFile(hFile, buffer, yaz0Archive->UnpackSize, &bytesWrite, NULL))
+			return E_EWRITE;
+	}
+	return 0;
+}
 //------------------------------------------------------------------------------
 int __stdcall ProcessFileW(HANDLE hArcData, int Operation, WCHAR* DestPath, WCHAR* DestName)
 {
@@ -130,6 +233,20 @@ int __stdcall ProcessFileW(HANDLE hArcData, int Operation, WCHAR* DestPath, WCHA
 			return E_EWRITE;
 	}
 	return 0;
+}
+
+//------------------------------------------------------------------------------
+void __stdcall SetChangeVolProc(HANDLE hArcData, tChangeVolProc pChangeVolProc)
+{
+	Yaz0Archive* yaz0Archive = (Yaz0Archive*)hArcData;
+	yaz0Archive->ChangeVolProc = pChangeVolProc;
+}
+
+//------------------------------------------------------------------------------
+void __stdcall SetProcessDataProc(HANDLE hArcData, tProcessDataProc pProcessDataProc)
+{
+	Yaz0Archive* yaz0Archive = (Yaz0Archive*)hArcData;
+	yaz0Archive->ProcessDataProc = pProcessDataProc;
 }
 
 //------------------------------------------------------------------------------
